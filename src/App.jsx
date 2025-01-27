48978055215ab32e1babe442bcd0b8a0399f6d7c
mavensagency <mavensagency@gmail.com>
2025-01-28 00:24:37 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
 import './App.css'
-import Footer from './components/Footer/footer'
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function App() {
